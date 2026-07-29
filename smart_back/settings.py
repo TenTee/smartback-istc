b6fa9583dfc9ad9abc3c5a4958e05b73f16bc3ab
FLAVEL690 <38857438+TenTee@users.noreply.github.com>
2026-07-29 16:17:37 +0100

    Update settings.py

--- smart_back/settings.py
+++ smart_back/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # Fichiers statiques & médias
 # -------------------------
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
